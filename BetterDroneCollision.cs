5a76ea9192fc6c4e5acc8b40a231c5545c326fa4
WhiteThunder <whetethunger@gmail.com>
2021-06-24 00:08:13 -0700

    Initial commit

---
+++ BetterDroneCollision.cs
@@ -0,0 +1,268 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using Oxide.Core;
+using Rust;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("Better Drone Collision", "WhiteThunder", "0.1.0")]
+    [Description("Overhauls drone collision damage.")]
+    internal class BetterDroneCollision : CovalencePlugin
+    {
+        #region Fields
+
+        private static Configuration _pluginConfig;
+
+        private const float ReplacementHurtVelocityThreshold = 1000;
+
+        private float? _vanillaHurtVelocityThreshold;
+
+        #endregion
+
+        #region Hooks
+
+        private void Init()
+        {
+            Unsubscribe(nameof(OnEntitySpawned));
+        }
+
+        private void OnServerInitialized()
+        {
+            foreach (var entity in BaseNetworkable.serverEntities)
+            {
+                var drone = entity as Drone;
+                if (drone == null)
+                    continue;
+
+                OnEntitySpawned(drone);
+            }
+
+            Subscribe(nameof(OnEntitySpawned));
+        }
+
+        private void Unload()
+        {
+            foreach (var entity in BaseNetworkable.serverEntities)
+            {
+                var drone = entity as Drone;
+                if (drone == null || !IsDroneEligible(drone))
+                    continue;
+
+                ResetDrone(drone);
+            }
+
+            _pluginConfig = null;
+        }
+
+        private void OnEntitySpawned(Drone drone)
+        {
+            if (!IsDroneEligible(drone))
+                return;
+
+            if (_vanillaHurtVelocityThreshold == null)
+                _vanillaHurtVelocityThreshold = drone.hurtVelocityThreshold;
+
+            // Delay to give other plugins a moment to cache the drone id so they can block this.
+            NextTick(() =>
+            {
+                if (drone != null)
+                    TryReplaceDroneCollision(drone);
+            });
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        private static bool DroneCollisionReplaceWasBlocked(Drone drone)
+        {
+            object hookResult = Interface.CallHook("OnDroneCollisionReplace", drone);
+            return hookResult is bool && (bool)hookResult == false;
+        }
+
+        private static bool IsDroneEligible(Drone drone) => !(drone is DeliveryDrone);
+
+        private static bool TryReplaceDroneCollision(Drone drone)
+        {
+            if (DroneCollisionReplaceWasBlocked(drone))
+                return false;
+
+            drone.hurtVelocityThreshold = ReplacementHurtVelocityThreshold;
+            drone.gameObject.AddComponent<DroneCollisionReplacer>();
+            return true;
+        }
+
+        private void ResetDrone(Drone drone)
+        {
+            if (drone.hurtVelocityThreshold == ReplacementHurtVelocityThreshold
+                && _vanillaHurtVelocityThreshold != null)
+            {
+                drone.hurtVelocityThreshold = (float)_vanillaHurtVelocityThreshold;
+            }
+
+            Puts($"{drone.hurtVelocityThreshold}");
+
+            var component = drone.GetComponent<DroneCollisionReplacer>();
+            if (component == null)
+                return;
+
+            UnityEngine.Object.Destroy(component);
+        }
+
+        #endregion
+
+        #region Collision Replacer
+
+        private class DroneCollisionReplacer : EntityComponent<Drone>
+        {
+            private float _nextDamageTime;
+
+	        private void OnCollisionEnter(Collision collision)
+            {
+                if (collision == null || collision.gameObject == null)
+                    return;
+
+                if (Time.time < _nextDamageTime)
+                    return;
+
+                var magnitude = collision.relativeVelocity.magnitude;
+                if (magnitude < _pluginConfig.MinCollisionVelocity)
+                    return;
+
+                var damage = magnitude * _pluginConfig.CollisionDamageMultiplier;
+                baseEntity.Hurt(damage, DamageType.Collision);
+                Interface.CallHook("OnDroneCollisionImpact", baseEntity, collision);
+                _nextDamageTime = Time.time + _pluginConfig.MinTimeBetweenImpacts;
+            }
+        }
+
+        #endregion
+
+        #region Configuration
+
+        private class Configuration : SerializableConfiguration
+        {
+            [JsonProperty("MinCollisionVelocity")]
+            public float MinCollisionVelocity = 3;
+
+            [JsonProperty("MinTimeBetweenImpacts")]
+            public float MinTimeBetweenImpacts = 0.25f;
+
+            [JsonProperty("CollisionDamageMultiplier")]
+            public float CollisionDamageMultiplier = 1;
+        }
+
+        private Configuration GetDefaultConfig() => new Configuration();
+
+        #endregion
+
+        #region Configuration Boilerplate
+
+        private class SerializableConfiguration
+        {
+            public string ToJson() => JsonConvert.SerializeObject(this);
+
+            public Dictionary<string, object> ToDictionary() => JsonHelper.Deserialize(ToJson()) as Dictionary<string, object>;
+        }
+
+        private static class JsonHelper
+        {
+            public static object Deserialize(string json) => ToObject(JToken.Parse(json));
+
+            private static object ToObject(JToken token)
+            {
+                switch (token.Type)
+                {
+                    case JTokenType.Object:
+                        return token.Children<JProperty>()
+                                    .ToDictionary(prop => prop.Name,
+                                                  prop => ToObject(prop.Value));
+
+                    case JTokenType.Array:
+                        return token.Select(ToObject).ToList();
+
+                    default:
+                        return ((JValue)token).Value;
+                }
+            }
+        }
+
+        private bool MaybeUpdateConfig(SerializableConfiguration config)
+        {
+            var currentWithDefaults = config.ToDictionary();
+            var currentRaw = Config.ToDictionary(x => x.Key, x => x.Value);
+            return MaybeUpdateConfigDict(currentWithDefaults, currentRaw);
+        }
+
+        private bool MaybeUpdateConfigDict(Dictionary<string, object> currentWithDefaults, Dictionary<string, object> currentRaw)
+        {
+            bool changed = false;
+
+            foreach (var key in currentWithDefaults.Keys)
+            {
+                object currentRawValue;
+                if (currentRaw.TryGetValue(key, out currentRawValue))
+                {
+                    var defaultDictValue = currentWithDefaults[key] as Dictionary<string, object>;
+                    var currentDictValue = currentRawValue as Dictionary<string, object>;
+
+                    if (defaultDictValue != null)
+                    {
+                        if (currentDictValue == null)
+                        {
+                            currentRaw[key] = currentWithDefaults[key];
+                            changed = true;
+                        }
+                        else if (MaybeUpdateConfigDict(defaultDictValue, currentDictValue))
+                            changed = true;
+                    }
+                }
+                else
+                {
+                    currentRaw[key] = currentWithDefaults[key];
+                    changed = true;
+                }
+            }
+
+            return changed;
+        }
+
+        protected override void LoadDefaultConfig() => _pluginConfig = GetDefaultConfig();
+
+        protected override void LoadConfig()
+        {
+            base.LoadConfig();
+            try
+            {
+                _pluginConfig = Config.ReadObject<Configuration>();
+                if (_pluginConfig == null)
+                {
+                    throw new JsonException();
+                }
+
+                if (MaybeUpdateConfig(_pluginConfig))
+                {
+                    LogWarning("Configuration appears to be outdated; updating and saving");
+                    SaveConfig();
+                }
+            }
+            catch
+            {
+                LogWarning($"Configuration file {Name}.json is invalid; using defaults");
+                LoadDefaultConfig();
+            }
+        }
+
+        protected override void SaveConfig()
+        {
+            Log($"Configuration changes saved to {Name}.json");
+            Config.WriteObject(_pluginConfig, true);
+        }
+
+        #endregion
+    }
+}
---
+++ README.md
@@ -0,0 +1,66 @@
+## Features
+
+- Overhauls drone collision damage so it's less extreme
+
+## Configuration
+
+Default configuration:
+
+```json
+{
+  "MinCollisionVelocity": 3.0,
+  "MinTimeBetweenImpacts": 0.25,
+  "CollisionDamageMultiplier": 1.0
+}
+```
+
+- `MinCollisionVelocity` -- Minimum collision velocity required to apply damage.
+  - When the collision velocity is lower than this amount, the collision is ignored and no damage is applied.
+- `MinTimeBetweenImpacts` -- Minimum time in seconds between drone collision impacts.
+  - If the drone collides multiple times in this time frame above the velocity threshold, only the first collision will count. This mechanic is very useful for larger drones since it prevents them from taking unreasonable damage when colliding with multiple objects at once.
+- `CollisionDamageMultiplier` -- Damage multiplier to apply to eadch collision impact above the velocity threshold.
+  - When using a value of `1.0`, the damage dealt will match the collision velocity.
+
+## FAQ
+
+#### How do I get a drone?
+
+As of this writing, RC drones are a deployable item named `drone`, but they do not appear naturally in any loot table, nor are they craftable. However, since they are simply an item, you can use plugins to add them to loot tables, kits, GUI shops, etc. Admins can also get them with the command `inventory.give drone 1`, or spawn one in directly with `spawn drone.deployed`.
+
+#### How do I remote-control a drone?
+
+If a player has building privilege, they can pull out a hammer and set the ID of the drone. They can then enter that ID at a computer station and select it to start controlling the drone. Controls are `W`/`A`/`S`/`D` to move, `shift` (sprint) to go up, `ctrl` (duck) to go down, and mouse to steer.
+
+Note: If you are unable to steer the drone, that is likely because you have a plugin drawing a UI that is grabbing the mouse cursor. The Movable CCTV was previously guilty of this and was patched in March 2021.
+
+## Recommended compatible plugins
+
+- [Drone Hover](https://umod.org/plugins/drone-hover) -- Allows RC drones to hover in place while not being controlled.
+- [Drone Lights](https://umod.org/plugins/drone-lights) -- Adds controllable search lights to RC drones.
+- [Drone Storage](https://umod.org/plugins/drone-storage) -- Allows players to deploy a small stash to RC drones.
+- [Drone Turrets](https://umod.org/plugins/drone-turrets) -- Allows players to deploy auto turrets to RC drones.
+- [Drone Effects](https://umod.org/plugins/drone-effects) -- Adds collision effects and propeller animations to RC drones.
+- [Auto Flip Drones](https://umod.org/plugins/auto-flip-drones) -- Auto flips upside-down RC drones when a player takes control.
+- [RC Identifier Fix](https://umod.org/plugins/rc-identifier-fix) -- Auto updates RC identifiers saved in computer stations to refer to the correct entity.
+
+## Developer Hooks
+
+#### OnDroneCollisionReplace
+
+```csharp
+bool? OnDroneCollisionReplace(Drone drone)
+```
+
+- Called when this plugin is about to alter a drone to replace its collision behavior
+  - This happens when drones spawn, and for existing drones when the plugin loads
+- Returning `false` will prevent the the drone from being altered
+- Returning `null` will result in the default behavior
+
+#### OnDroneCollisionImpact
+
+```csharp
+void OnDroneCollisionImpact(Drone drone, Collision collision)
+```
+
+- Called when a drone collision occurs above the velocity threshold
+- No return behavior
